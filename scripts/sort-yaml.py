4f20279ae7b0053c9587d9c5ecd3beff9ffa6991
Myles Braithwaite <me@mylesbraithwaite.com>
2016-11-18 10:59:10 -0500

    Moving the sort script to Python. [0]

--- scripts/sort-yaml.py
+++ scripts/sort-yaml.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
-from os import chdir
-from glob import iglob
+# from os import chdir
+# from glob import iglob
+from collections import OrderedDict
 from os.path import dirname, realpath, join
 
 import yaml
@@ -12,22 +13,22 @@ def sort_yaml_file(yaml_file):
         raw = fobj.read()
         data = yaml.load(raw)
 
-    data.sort()
-
-    return yaml.dump(data)
+    return yaml.dump(data, default_flow_style=False, explicit_start=True)
 
 
 def main():
     data_dir = join(dirname(realpath(__file__)), '../source/_data')
 
-    chdir(data_dir)
-    yaml_files = iglob('*.yml')
+    # chdir(data_dir)
+    # yaml_files = iglob('*.yml')
+
+    yaml_files = [join(data_dir, 'tags.yml')]
 
     for yaml_file in yaml_files:
         raw = sort_yaml_file(yaml_file)
 
         with open(yaml_file, 'w') as fobj:
-            fobj.write(raw)
+            fobj.write(raw.replace('\n-', '\n\n-'))
 
 
 if __name__ == '__main__':
