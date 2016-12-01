f5b71442091c74d1f501efbc242b4923fb8bdfcb
Myles Braithwaite <me@mylesbraithwaite.com>
2016-12-01 14:46:05 -0500

    Did some work.

--- script/tags.py
+++ script/tags.py
@@ -9,15 +9,16 @@ import click
 import frontmatter
 
 
-def tag_meta(tag_name):
-    with open(join(dirname(realpath(__file__)),
-                   '../source/_data/tags.yml'), 'r') as fobj:
-        meta = yaml.load(fobj.read())
-
-    return meta.get(tag_name, {'name': tag_name})
-
-
 def post_list():
+    """
+    A list of the Jekyll posts.
+
+    Returns
+    -------
+    list
+        A list of frontmatter.Post objects.
+    """
+
     posts_dir = join(dirname(realpath(__file__)), '../source/_posts')
 
     post_files = glob(join(posts_dir, '**', '*'))
@@ -32,15 +33,48 @@ def post_list():
     return post_list
 
 
-@click.group()
-def cli():
-    pass
+def tag_yml():
+    """
+    Converts the `./source/_data/tags.yml` file to a Python dict.
+
+    Returns
+    -------
+    dict
+        The contents of the `./source/_data/tags.yml` file convert to a dict.
+    """
+    with open(join(dirname(realpath(__file__)),
+                   '../source/_data/tags.yml'), 'r') as fobj:
+        return yaml.load(fobj.read())
 
 
-@cli.command('list')
+def tag_meta(slug):
+    """
+    Get the meta information of a specific tag.
+
+    Paramerters
+    -----------
+    slug : str
+        The slug of the tag.
+
+    Returns
+    -------
+    dict
+        The meta data of the tag.
+    """
+    meta = tag_yml()
+
+    return meta.get(slug, {'name': slug})
+
+
 def tag_list():
-    """List all the tags."""
+    """
+    Get all the tags that are currently used in posts.
 
+    Returns
+    -------
+    list
+        A list of dict with tag meta data.
+    """
     tag_list = []
 
     for post in post_list():
@@ -48,35 +82,73 @@ def tag_list():
 
     tag_list.sort()
 
-    resutls = []
+    resp = []
 
     for tag, group in groupby(tag_list):
-        count = len(list(group))
-        resutls.append('{tag} ({count})'.format(tag=tag, count=count))
+        resp.append({
+            'name': tag,
+            'count': len(list(group)),
+            # 'posts': posts
+        })
 
-    click.echo_via_pager('\n'.join(resutls))
+    return resp
 
 
-@cli.command('get')
-@click.argument('name', metavar='<name>')
-@click.option('--edit', is_flag=True)
-def tag_get(name, edit):
-    """Get information about a tag."""
+def tag_get(slug):
+    """
+    Get information about an individual tag.
 
-    tag_info = tag_meta(name)
+    Paramerters
+    -----------
+    slug : str
+        The slug of the tag.
+
+    Returns
+    -------
+    list
+        A list of dict with the tag meta data.
+    """
+    tag_info = tag_meta(slug)
     tag_info['posts'] = []
 
     for post in post_list():
-        if any(tag == name for tag in post['tags']):
+        if any(tag == slug for tag in post['tags']):
             tag_info['posts'].append(post)
 
-    click.secho('{name}'.format(**tag_info), fg='green')
+    return tag_info
 
-    for post in tag_info['posts']:
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.command('list')
+def cli_list():
+    """List all the tags."""
+
+    resp = []
+
+    for tag in tag_list():
+        resp.append('{name} ({count})'.format(**tag))
+
+    click.echo_via_pager('\n'.join(resp))
+
+
+@cli.command('get')
+@click.argument('slug', metavar='<slug>')
+@click.option('--edit', is_flag=True)
+def cli_get(slug, edit):
+    """Get information about a tag."""
+    tag = tag_get(slug)
+
+    click.secho('{name}'.format(**tag), fg='green')
+
+    for post in tag['posts']:
         click.echo('{title} - {file_path}'.format(**post))
 
     if edit:
-        for post in tag_info['posts']:
+        for post in tag['posts']:
             click.edit(filename=post['file_path'])
 
 
--- source/_posts/article/2010-02-02-linode-wiki.markdown
+++ source/_posts/article/2010-02-02-linode-wiki.markdown
@@ -3,7 +3,7 @@ layout: post_article
 category: article
 date: 2010-02-02T00:00:00
 title: "Got another Linode & Moved the Wiki"
-tags: [linode, system-administration, munin, wiki, server, hatta]
+tags: [linode, sysadmin, munin, wiki, server, hatta]
 ---
 
 I ordered a second [Linode](http://www.linode.com/?r=489fd5146a3553db67138302f6c6d44a029cf45a "A VPS Provider") (a 360) on the weekend for some PHP applications ([Fever](http://feedafever.com/ "An online feed reader.") and [DokuWiki](http://dokuwiki.org/ "A super simple and powerful wiki engine.")). I have been with many VPS services and [Linode](http://www.linode.com/?r=489fd5146a3553db67138302f6c6d44a029cf45a "A VPS Provider") is by far the best you can buy (shared host I will have to say [Nearly Free Speech](https://www.nearlyfreespeech.net/ "NearlyFreeSpeech.NET Web Hosting")). It is hosted in the same data centre (Newark, NJ) so I have an internal IP address connecting both of them. I am running [Nagios](http://www.nagios.org/) and [Munin](http://munin.sourceforge.net/) on both computers ([Panda is monitoring Fox](http://panda.mylesbraithwaite.com/munin/mylesbraithwaite.com/fox.mylesbraithwaite.com.html) and [Fox is monitoring Panda](http://fox.mylesbraithwaite.com/munin/mylesbraithwaite.com/panda.mylesbraithwaite.com.html)) that way if one goes down the other will notify me.
