1038af1b357955a92bfdd87e25ac0f08e019a197
Myles Braithwaite <me@mylesbraithwaite.com>
2017-01-09 14:17:06 -0500

    :rocket: Fixing the S3 push and pull tasks.
    Signed-off-by: Myles Braithwaite <me@mylesbraithwaite.com>

--- fabfile/s3.py
+++ fabfile/s3.py
@@ -5,11 +5,23 @@ from fabric.api import env, task, local
 
 
 @task
-def sync():
-    """Sync the uploads directory."""
+def push():
+    """Push all uploads to S3."""
     config = {
         'local': join(env.root_dir, 'source/uploads'),
         'remote': 's3://assets.mylesbraithwaite.com/uploads/',
+        'exclude': '".DS_Store"'
+    }
+
+    local('aws s3 sync {local} {remote} --exclude {exclude}'.format(**config))
+
+
+@task
+def pull():
+    """Pull all uploads to S3."""
+    config = {
+        'local': 's3://assets.mylesbraithwaite.com/uploads/',
+        'remote': join(env.root_dir, 'source/uploads'),
         'exclude': '".DS_Store"'
     }
 
